28eca83e55d73c3e9b08fcfd488571b7c81fc3ee
Trevor Erwin <erwintj@mail.uc.edu>
2019-04-05 10:46:07 -0400

    Display leading bidder in project component

--- src/components/project/index.js
+++ src/components/project/index.js
@@ -5,6 +5,7 @@ import { Box, Text } from 'grommet';
 const Project = ({
   data: {
     amount,
+    buyer,
     id,
     reqs,
     title,
@@ -23,7 +24,7 @@ const Project = ({
       <h1>{title}</h1>
       <Text>Requirements: {reqs}</Text>
       <Text>Time to complete: {hours} hours</Text>
-      {amount && <Text>Current Bid: ${amount}{(type === 'hourly' && '/hr')}</Text>}
+      {amount && <Text>Current Bid: ${amount}{(type === 'hourly' && '/hr')}{buyer && <Text> ({buyer})</Text>}</Text>}
       <Text>Expires: {expirationDate} @ {expirationTime}</Text>
       {
         (seller !== sessionStorage.getItem('username')) &&
